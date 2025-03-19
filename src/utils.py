4bb89154f822bedafca81fc52d9a5bb6ea13e1fd
belkercha <mbelkercha@yahoo.fr>
2025-03-19 22:27:46 +0100

    pipeline

--- src/utils.py
+++ src/utils.py
@@ -10,7 +10,6 @@ from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
-
 def save_object(file_path, obj):
     try:
         dir_path = os.path.dirname(file_path)
@@ -23,28 +22,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
     
-
 def evaluate_models(X_train, y_train,X_test,y_test,models,param):
-    try:
-        report={}
-        for i in range(len(list(models))):
-            model=list(models.values())[i]
-            para=param[list(model.keyy())][i]
-            
-            gs=GridSearchCV(model, para, cv=3)
-            gs.fit(X_train, y_train)
-            
-            y_train_pred=model.predict(X_train)
-            y_test_predict=model.predict(X_test)
-            
-            train_model_score=r2_score(y_train, y_train_pred)
-            test_model_score=r2_score(y_test, y_test_pred)
-            report[list(models.keys())[i]] = test_model_score
-            
-        return report
-    except:
-        pass
-    
     try:
         report = {}
 
@@ -71,5 +49,14 @@ def evaluate_models(X_train, y_train,X_test,y_test,models,param):
             report[list(models.keys())[i]] = test_model_score
 
         return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return pickle.load(file_obj)
+
     except Exception as e:
         raise CustomException(e, sys)
